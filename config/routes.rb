988dc8538a447c84bae25b40497941bc056e2cf4
Jacob Wheeler <wheeler.jacob.r@gmail.com>
2017-06-05 11:29:11 -0600

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
 
 
-  get 'products/index'
-
-  get 'products/show'
-
-  get 'products/edit'
-
-  get 'products/new'
-
   root to: 'home#index'
 
   devise_for :users
   resources :stores
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
