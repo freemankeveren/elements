ab084361204fb48fe47e5ba139abbb44c9ff1612
Jacob Wheeler <wheeler.jacob.r@gmail.com>
2017-06-05 12:21:58 -0600

    initial navbar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,23 @@
 class ProductsController < ApplicationController
+before_action :set_product
   def index
+    @products = Product.all
   end
 
   def show
+    # maybe filter
   end
 
-  def edit
-  end
+  # def edit
+  # end
+  #
+  # def new
+  # end
 
-  def new
+
+  private
+
+  def set_product
+    @product = current_user.products.find(params[:id])
   end
 end
