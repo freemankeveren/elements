dcc3d3d63a846e7dee525f56f209ab3f1831e296
Jacob Wheeler <wheeler.jacob.r@gmail.com>
2017-06-05 10:58:31 -0600

    product controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  get 'stores/index'
 
-  get 'stores/show'
 
-  get 'stores/edit'
+  get 'products/index'
 
-  get 'stores/new'
+  get 'products/show'
+
+  get 'products/edit'
+
+  get 'products/new'
 
   root to: 'home#index'
 
   devise_for :users
+  resources :stores
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170605160703) do
+ActiveRecord::Schema.define(version: 20170605163919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "stores", force: :cascade do |t|
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.float "price"
+    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
