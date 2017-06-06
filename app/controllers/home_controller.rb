e47862fc6f8477560f8739b7150cb4360c293d44
Jamie Parsons <jhelenparsons@gmail.com>
2017-06-05 22:21:23 -0600

    events

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,13 +8,21 @@ class HomeController < ApplicationController
     redirect_to root_path
   end
 
+  # , Materialize.toast('I am a toast!', 4000)
+# <a class="btn" onclick="Materialize.toast('I am a toast', 4000)">Toast!</a>
+  # if @car.save
+  #     redirect_to @car, notice: 'Car Created!' #rails knows you want to redirect to show pages; #or redirect to car_path_new ??
+  #   else
+  #
+  #     render :new
+
   def index
     @products = current_user.cart.products
   end
 
   def delete_from_cart
     cart = current_user.cart
-    
+
     cart.products.delete_if {|product| product.id.to_s == params[:id]}
     cart.save
     redirect_to root_path
